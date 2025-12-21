e47a95f305929fed6bdf264970c7903bf8c9f28f
Thitiwut Chutipongwanit <thitiwut.chu@gmail.com>
2025-12-22 00:41:18 +0700

    feat: add config

---
+++ src/config.rs
@@ -0,0 +1,24 @@
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    #[serde(default = "default_num_threads")]
+    num_threads: u8,
+    repositories: Vec<RepositoryConfig>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct RepositoryConfig {
+    dir: String,
+    branch: String,
+    #[serde(default = "default_checkout_prev_branch")]
+    checkout_prev_branch: bool,
+}
+
+fn default_num_threads() -> u8 {
+    4
+}
+
+fn default_checkout_prev_branch() -> bool {
+    true
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    let cfg_file_content = fs::read("./config.yaml").unwrap();
+
+    let cfg = serde_yaml::from_slice::<config::Config>(&cfg_file_content).unwrap();
+    println!("cfg: {cfg:?}");
 }
